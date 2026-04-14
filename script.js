44f3d01d0efd5f9e365f457a7b257634b6918240
Agrani Sinha <75300113+agranisinha@users.noreply.github.com>
2026-04-14 00:34:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-// SCROLL ANIMATION
-const items = document.querySelectorAll('.fade');
-
-window.addEventListener('scroll', () => {
-  items.forEach(el => {
-    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
-      el.classList.add('show');
-    }
+function showSection(id) {
+  document.querySelectorAll('.panel').forEach(sec => {
+    sec.classList.remove('active');
   });
-});
 
-// CURSOR GLOW
-document.addEventListener("mousemove", e => {
-  const glow = document.getElementById("cursorGlow");
-  glow.style.left = e.pageX + "px";
-  glow.style.top = e.pageY + "px";
-});
+  document.getElementById(id).classList.add('active');
+}
