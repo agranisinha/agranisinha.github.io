db33fedad9fffb6c1258213c92464f259378e588
Agrani Sinha <75300113+agranisinha@users.noreply.github.com>
2026-04-14 00:44:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function showSection(id) {
-  document.querySelectorAll('.panel').forEach(sec => {
-    sec.classList.remove('active');
-  });
-
+function openTab(id) {
+  document.querySelectorAll('.panel').forEach(p => p.classList.remove('active'));
   document.getElementById(id).classList.add('active');
+
+  const tabs = document.getElementById('tabs');
+
+  if (!document.querySelector(`[data-tab='${id}']`)) {
+    const tab = document.createElement('div');
+    tab.className = 'tab';
+    tab.innerText = id + ".js";
+    tab.setAttribute("data-tab", id);
+
+    tab.onclick = () => openTab(id);
+
+    tabs.appendChild(tab);
+  }
 }
