f97e61f037f6b6cbed8e70aaac519ea34deb0aab
Agrani Sinha <75300113+agranisinha@users.noreply.github.com>
2026-04-13 22:42:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-const elements = document.querySelectorAll(".fade-in");
+const items = document.querySelectorAll('.fade-in');
 
-function showOnScroll() {
-  const triggerBottom = window.innerHeight * 0.85;
-
-  elements.forEach(el => {
-    const boxTop = el.getBoundingClientRect().top;
-
-    if (boxTop < triggerBottom) {
-      el.classList.add("show");
+window.addEventListener('scroll', () => {
+  items.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add('show');
     }
   });
-}
-
-window.addEventListener("scroll", showOnScroll);
-showOnScroll();
+});
